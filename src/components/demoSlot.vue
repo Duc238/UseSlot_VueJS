ced130763cf69a890bcd1a390cd220aa68a3021c
unknown <vtnduc2382000@gmail.com>
2024-04-12 08:26:04 +0700

    first commit

---
+++ src/components/demoSlot.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <comp-tabs title="Tiêu đề của tab">
+            <ul class="tabs-header">
+                <li v-for="(item, index) in tabsHeader" v-bind:key="index"
+                    v-on:click="setActiveTab(index)"
+                >{{item.text}}</li>
+            </ul>
+            <div class="tabContentTab1" v-if="tabActive===0">
+                <p>Maragret Wang</p>
+            </div>
+            <div class="tabContentTab2" v-else-if="tabActive===1">
+                <p>Trần Hoài Ngọc</p>
+            </div>
+            <div class="tabContentTab3" v-else>
+                <p>Trần Mỹ Sương</p>
+            </div>
+        </comp-tabs>
+    </div>
+</template>
+<script>
+import CompTabs from "./CompTabs.vue"
+export default {
+    components:{
+        CompTabs
+    },
+    data(){
+        return{
+            tabsHeader:[
+                {text:"Tab1"},
+                {text:"Tab2"},
+                {text:"Tab3"}
+            ],
+            tabActive:0
+        }
+    },
+    methods:{
+        setActiveTab(index){
+            this.tabActive=index;
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
